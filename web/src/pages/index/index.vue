221933d842ab8dcd704f1de97e441a1a35ec3b84
light <978854603@qq.com>
2019-07-08 13:17:04 +0800

    修改首页

--- web/src/pages/index/index.vue
+++ web/src/pages/index/index.vue
@@ -14,22 +14,36 @@
       <div class="recommended-today boder">
         <div class="left">
           <h2 class="title">今日推荐</h2>
-          <div class="today-news">感恩陈总缘系统如何带好团队</div>
+          <div class="today-news">{{data.recommendation.name}}</div>
         </div>
         <div class="right">
           <van-icon name="like" />
         </div>
       </div>
-      <div class="boder">
+      <div class="news boder">
         <h2 class="title">新闻</h2>
-
+        <ul>
+          <li v-for="item in data.news" :key="item.name">
+            {{item.name}}
+          </li>
+        </ul>
       </div>
-      <div class="boder">
+      <div class="recommended-read boder">
         <h2 class="title">推荐阅读</h2>
+        <ul>
+          <li v-for="item in data.recommendedReads" :key="item.name">
+            {{item.name}}
+          </li>
+        </ul>
 
       </div>
-      <div class="boder">
+      <div class="hot-top boder">
         <h2 class="title">热点专题</h2>
+        <ul>
+          <li v-for=" hot in data.hotTops" :key="hot.name">
+            {{hot.name}}
+          </li>
+        </ul>
       </div>
     </div>
   </div>
@@ -50,8 +64,27 @@ export default {
       autoplay: false,
       interval: 5000,
       duration: 1000,
-      userInfo: {
-
+      data: {
+        recommendation: {
+          name: '感恩陈总缘系统如何带好团队',
+          icon: '',
+          date: ''
+        },
+        news: [
+          { name: '技术选型的4个参考原则', },
+          { name: '从大公司转型创业的七个采坑经验', },
+          { name: '大数据赋能智慧城市建设' },
+          { name: '前端技术未来展望之路' }
+        ],
+        recommendedReads: [
+          { name: '代码规范，代码长治久安的基础' },
+          { name: '从工程师到管理者我的思考与实现' },
+          { name: '稳定性实践：开关和预案' },
+        ],
+        hotTops: [
+          { name: '前端训练营' },
+          { name: '即可时间训练营' }
+        ]
       }
     }
   },
